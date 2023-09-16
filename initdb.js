6d497b8ad6ad64cf85b52619bcf372d303e31df8
Filippo Bellei <filippobellei@gmail.com>
2023-09-16 13:48:33 +0200

    Improve initdb readability

--- initdb.js
+++ initdb.js
@@ -5,10 +5,10 @@ const sql =
     'CREATE TABLE task (id INTEGER PRIMARY KEY, name TEXT, description TEXT);';
 
 // Initialize the database
-db.run(sql, (error) => {
-    if (error) {
+db.run(sql, (err) => {
+    if (err) {
         console.error('Error during database initialization:');
-        console.error(error);
+        console.error(err);
         return;
     }
     console.log('Database initialized');
